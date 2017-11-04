9f65a87ce70d6c3a6eb9195a120f5ed88b0658e5
root <joepap981@gmail.com>
2017-11-04 22:08:36 +0900

    "Make MySQL replication into role"

--- install_small_deployment.sh
+++ install_small_deployment.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles
-export ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library
-export ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do
-
 #install datastores server
 echo "Now installing datastores server..."
 sudo ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do  ansible-playbook -i inventory.ini -e server_var.yml playbooks/edx-datastores.yml -vvvv
