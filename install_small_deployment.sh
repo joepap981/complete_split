ab45d57214bd181ba70adf01d6a62bb281e0a981
root <root@ip-10-0-0-134.ap-northeast-2.compute.internal>
2017-11-07 18:12:47 +0000

    "Made address binding into role"

--- install_small_deployment.sh
+++ install_small_deployment.sh
@@ -2,8 +2,8 @@
 
 #install datastores server
 echo "Now installing datastores server..."
-sudo ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do  ansible-playbook -i inventory.ini -e server_var.yml playbooks/edx-datastores.yml -vvvv
+sudo ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do  ansible-playbook -i inventory.ini -e server_var.yml playbooks/edx-datastores.yml
 
 #install stateless server
 echo "Now installing Stateless server..."
-sudo ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do  ansible-playbook -i inventory.ini -e server_var.yml playbooks/edx-stateless.yml -vvvv
+sudo ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do  ansible-playbook -i inventory.ini -e server_var.yml playbooks/edx-stateless.yml
